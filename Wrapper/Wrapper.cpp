cca8a3b253f39702e2c4673c3832bb47a7ac832a
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-27 21:22:02 +0900

    ビュークラスがエラーになるので暫定的に対応

--- Wrapper/Wrapper.cpp
+++ Wrapper/Wrapper.cpp
@@ -11,6 +11,53 @@
 namespace  SampleWrapper  {
 namespace  Common  {
 
+//----------------------------------------------------------------
+//    インスタンスを初期化する
+//  （デフォルトコンストラクタ）。
+//
+
+SampleDocument::SampleDocument()
+{
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （デストラクタ）。
+//
+
+SampleDocument::~SampleDocument()
+{
+    this->!SampleDocument();
+}
+
+//----------------------------------------------------------------
+//    インスタンスを破棄する
+//  （ファイナライザ）。
+//
+
+SampleDocument::!SampleDocument()
+{
+}
+
+//----------------------------------------------------------------
+//    入力メッセージ中に含まれるアルファベットを数える。
+//
+
+int
+SampleDocument::countAlphabet()
+{
+    return ( 0 );
+}
+
+//----------------------------------------------------------------
+//    メッセージを設定する。
+//
+
+void
+SampleDocument::setMessage(
+        System::String^ message)
+{
+}
 
 }   //  End of namespace  Common
 }   //  End of namespace  SampleWrapper
