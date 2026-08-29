bc5c27446cc040797f42ce9e17fd1323affd7261
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-29 11:38:51 +0900

    メインウィンドウをデザイン

--- View/Views/MainWindow.xaml.vb
+++ View/Views/MainWindow.xaml.vb
@@ -17,15 +17,11 @@ Namespace Global.BaseballScoreView.Views
 
 Public Class MainWindow
 
-Private m_model As Models.MySampleModel
-
 Public Sub New()
 ''--------------------------------------------------------------------
 ''    コンストラクタ
 ''--------------------------------------------------------------------
     InitializeComponent()
-    Me.m_model = New Models.MySampleModel()
-    SampleControl1.ViewModel = New WpfControl.Sample.SampleViewModel(m_model)
 End Sub
 
 
