a19914277dc9b40b59d514cd787d503706aa6e20
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-29 11:10:21 +0900

    リファクタリング
    (1) 無視リストを更新
    (2) ファイルをサブディレクトリに移動
    (3) 移動に合わせて名前空間を修正
    (4) デバッガの設定を変更

---
+++ View/Models/MySampleModel.vb
@@ -0,0 +1,46 @@
+﻿''  -*-  coding: utf-8-with-signature-unix     -*-  ''
+''************************************************************************
+''                                                                      ''
+''                  ---  Baseball  Score  Project  ---                  ''
+''                                                                      ''
+''          Copyright (C), 2017-2026, Takahiro Itou                     ''
+''          All Rights Reserved.                                        ''
+''                                                                      ''
+''          License: (See COPYING or LICENSE files)                     ''
+''          GNU Affero General Public License (AGPL) version 3,         ''
+''          or (at your option) any later version.                      ''
+''                                                                      ''
+''************************************************************************
+
+Imports WpfControl.Sample
+
+Namespace Global.BaseballScoreView.Models
+
+Public Class MySampleModel
+        Inherits WpfControl.Sample.AbstractSampleModel
+
+Public Overrides Sub executeCommand()
+''--------------------------------------------------------------------
+''    適当な動作を実行する
+''--------------------------------------------------------------------
+    RunCount(Me.InputText)
+End Sub
+
+
+Private Sub runCount(ByVal message As String)
+''--------------------------------------------------------------------
+''    サンプル動作
+''
+''    入力テキスト中のアルファベットの個数を数える
+''--------------------------------------------------------------------
+Dim outText As String
+
+    outText = $"Not Implemented {message}"
+    Me.setOutputText(outText)
+    MsgBox(outText, MsgBoxStyle.OkOnly)
+End Sub
+
+
+End Class
+
+End Namespace
---
+++ View/Views/MainWindow.xaml.vb
@@ -0,0 +1,34 @@
+﻿''  -*-  coding: utf-8-with-signature-unix     -*-  ''
+''************************************************************************
+''                                                                      ''
+''                  ---  Baseball  Score  Project  ---                  ''
+''                                                                      ''
+''          Copyright (C), 2017-2026, Takahiro Itou                     ''
+''          All Rights Reserved.                                        ''
+''                                                                      ''
+''          License: (See COPYING or LICENSE files)                     ''
+''          GNU Affero General Public License (AGPL) version 3,         ''
+''          or (at your option) any later version.                      ''
+''                                                                      ''
+''************************************************************************
+
+
+Namespace Global.BaseballScoreView.Views
+
+Public Class MainWindow
+
+Private m_model As Models.MySampleModel
+
+Public Sub New()
+''--------------------------------------------------------------------
+''    コンストラクタ
+''--------------------------------------------------------------------
+    InitializeComponent()
+    Me.m_model = New Models.MySampleModel()
+    SampleControl1.ViewModel = New WpfControl.Sample.SampleViewModel(m_model)
+End Sub
+
+
+End Class
+
+End Namespace
