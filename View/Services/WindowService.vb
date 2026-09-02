d01f2433b8fbf4cd9644ca4ef21395c88a752d5b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-02 22:18:53 +0900

    サービスクラスを新規作成

---
+++ View/Services/WindowService.vb
@@ -0,0 +1,31 @@
+﻿''  -*-  coding: utf-8-with-signature-unix     -*-  ''
+''************************************************************************
+''                                                                      ''
+''                  ---  Baseball  Score  Project  ---                  ''
+''                                                                      ''
+''          Copyright (C), 2017-2026, Takahiro Itou                     ''
+''          All Rights Reserved.                                        ''
+''                                                                      ''
+''          License: (See COPYING or LICENSE files)                     ''
+''          GNU Affero General Public License (AGPL) version 3,         ''
+''          or (at your option) any later version.                      ''
+''                                                                      ''
+''************************************************************************
+
+Imports BaseballScoreHelper.Services
+Imports BaseballScoreHelper.ViewModels
+
+Imports BaseballScoreView.Views
+
+Public Class WindowService
+        Implements IWindowService
+
+Public Sub showLineView(ByVal viewModel As VictoryLineViewModel) _
+        Implements IWindowService.showLineView
+
+    Dim frmLine As New LineView()
+    frmLine.DataContext = viewModel
+    frmLine.ShowDialog()
+End Sb
+
+End Class
